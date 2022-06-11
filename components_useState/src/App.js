f7b6c69c5b2954b1e41d6ab0273443834a1856f6
Omar Ramirez <ojramirezv@gmail.com>
2022-06-10 19:14:28 -0500

    uploading persona component

--- components_useState/src/App.js
+++ components_useState/src/App.js
@@ -1,11 +1,15 @@
 import "./styles.css";
-import Usuario from "./componets/usuario";
+/*import Usuario from "./componets/usuario";
 import Usuario2 from "./componets/usuario";
 import Usuario3 from "./componets/usuario";
 import {useState} from "react";
+*/
+
+import Persona from "./componets/persona";
 const source = "https://www.breakingbadapi.com/api/characters";
 
 export default function App() {
+  /*
   const listaUsuarios = ["omar", "renzo", "sergio"];
   const [lista, setLista] = useState([]);
   //let lista = [];
@@ -35,23 +39,12 @@ export default function App() {
   if (lista.length === 0) {
     return <div>There is not information yet</div>;
   }
-
+*/
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <Usuario2 nombreUsuario={"camila"} nomUsu={"renzo"} />
-      <Usuario2 nombrecito={"camila"} />
-      {listaUsuarios.join("-")};
-      {lista.map((usuario) => {
-        return (
-          <div>
-            {usuario.name}
-            <Usuario3 username={usuario.name} picture={usuario.picture} nombreUsuario={usuario.name}/>
-          </div>
-        );
-      })}
-      ;
+      <Persona perName = {"OMAR"}/>
     </div>
   );
+  
+ 
 }
---
+++ components_useState/src/componets/persona.js
@@ -0,0 +1,6 @@
+const Persona = (props) => {
+    return <div>{props.perName}</div>;
+    
+};
+
+export default Persona;
