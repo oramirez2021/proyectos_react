b2d133002c8a46cfaa933b788a99e6bf2fdb50a4
OMAR <ojramirezv@gmail.com>
2022-06-13 07:27:59 -0500

    modifications

--- components_useState/src/App.js
+++ components_useState/src/App.js
@@ -4,11 +4,12 @@ import Usuario2 from "./componets/usuario";
 import Usuario3 from "./componets/usuario";
 import {useState} from "react";
 */
-
 import Objeto from "./componets/persona";
-
-
+import {useSate, useState} from "react";
 export default function App() {
+  const names = ["omar","renzo","sergio","michelle", "ivonne","raul"];
+  const source = "https://www.breakingbadapi.com/api/characters";
+  const [castingList, setCastingList] = useState([]);
   /*
   const listaUsuarios = ["omar", "renzo", "sergio"];
   const [lista, setLista] = useState([]);
@@ -40,11 +41,22 @@ export default function App() {
     return <div>There is not information yet</div>;
   }
 */
-  return (
-    <div className="App">
-      <Objeto perName = {"OMAR"} text="Omar Ramirez Vargas"/>
-    </div>
-  );
-  
- 
+const listaActores = fetch(source).then((response)=>{
+  if(response.status === 200){
+    response.json().then((datos)=>{
+      const newListaActores= datos.map((actor)=>{
+        return{nombre:actor.name,
+        foto:actor.img}
+      })
+      setCastingList(listaActores);
+      console.log(castingList);
+    })
+  }
+})
+const array = ["omar","renzo","sergio","michelle"];
+array.map((nombre)=>{
+  console.log(nombre);
+})
+return <Objeto perName = {"OMAR"} text = {"JONA"}  />
+
 }
