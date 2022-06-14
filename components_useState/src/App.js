ed7f33768c25c05cbe0f1973b1053359c34b746e
OMAR <ojramirezv@gmail.com>
2022-06-14 06:50:15 -0500

    updating useState and useEfect example

--- components_useState/src/App.js
+++ components_useState/src/App.js
@@ -1,62 +1,52 @@
 import "./styles.css";
-/*import Usuario from "./componets/usuario";
-import Usuario2 from "./componets/usuario";
-import Usuario3 from "./componets/usuario";
-import {useState} from "react";
-*/
 import Objeto from "./componets/persona";
-import {useSate, useState} from "react";
+import {useState, useEffect} from "react";
+
 export default function App() {
   const names = ["omar","renzo","sergio","michelle", "ivonne","raul"];
   const source = "https://www.breakingbadapi.com/api/characters";
   const [castingList, setCastingList] = useState([]);
-  /*
-  const listaUsuarios = ["omar", "renzo", "sergio"];
-  const [lista, setLista] = useState([]);
-  //let lista = [];
-  const characters = fetch(source)
-    .then((response) => {
-      if (response.status === 200) {
-        console.log("everything is going good");
-        response.json().then((data) => {
-          console.log("DATADATADATA");
-          console.log(data);
-          const newLista = data.map((character) => {
-            return {username:character.name,
-              picture:character.img
-          }});
-          setLista(newLista);
-          console.log("OMAR");
-          console.log(lista);
-          
-        });
+  const [counter, setCounter] = useState(0);
+  useEffect(()=>{
+    console.debug("fist load of the user list");
+    fetch(source).then((response)=>{
+      if(response.status === 200){
+        response.json().then((datos)=>{
+          console.log("Received the user list");
+          const newListaActores= datos.map((actor)=>{
+            return{nombre:actor.name,
+            imagen:actor.img}
+          })
+          setCastingList(newListaActores);
+          console.log(castingList);
+        })
       }
-      console.log(response);
     })
-    .catch((error) => {
-      console.log(error);
-    });
-
-  if (lista.length === 0) {
-    return <div>There is not information yet</div>;
-  }
-*/
-const listaActores = fetch(source).then((response)=>{
-  if(response.status === 200){
-    response.json().then((datos)=>{
-      const newListaActores= datos.map((actor)=>{
-        return{nombre:actor.name,
-        foto:actor.img}
-      })
-      setCastingList(listaActores);
-      console.log(castingList);
-    })
-  }
-})
-const array = ["omar","renzo","sergio","michelle"];
+  },[]);
+  useEffect(()=>{
+    console.debug("use list loaded");
+  },[castingList]);
+ 
+/*const array = ["omar","renzo","sergio","michelle"];
 array.map((nombre)=>{
   console.log(nombre);
-})
-return <Objeto perName = {"OMAR"} text = {"JONA"}  />
+})*/
+
+console.log("castingList has data");
+return (
+<div className="App">
+    <div>Total:{counter}<p>
+      <button onClick={(event)=>{setCounter(counter+1)}}>Add 1</button>
+      </p>
+    </div>
+    {castingList.map((act)=>(
+      <Objeto perName = {act.nombre} source = {act.imagen} />
+
+    ))}
+</div>
+)
+
+
+//return <Objeto perName = {"OMAR"} text = {"JONA"} src = {source} />
 
 }
--- components_useState/src/componets/persona.js
+++ components_useState/src/componets/persona.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 const textStyle = {
     border:"5px solid green",
     marginBottom:"10px",
@@ -7,17 +9,31 @@ const Persona = (props) => {
     return <div style={textStyle}>{props.perName}</div>;
     
 };
+class NombreC extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            name:props.perName
+        };
+    }
+    render(){
+        return <div>{this.props.perName}</div>
+    }
+}
 const Foto = (props)=>{
-    const source = `https://via.placeholder.com/150/0000FF/808080?text=${props.text}`;
+    //const source = `https://via.placeholder.com/150/0000FF/808080?text=${props.text}`;
     return <div>
-        <img src={source} width={"190"} height={"190"}></img>
+        <img src={props.source} width={"190"} height={"190"} alt={props.actorname}></img>
     </div>;
 }
 const Objeto = (props)=>{
     return(<div>
-        <Persona perName={props.perName}></Persona>
-        <Foto text={props.text} ></Foto>
+        <NombreC perName={props.perName}/>
+        <Foto source={props.source} actorname={props.perName}></Foto>
     </div>);
    
 }
+
+
+
 export default Objeto;
