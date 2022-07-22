201fc1d5b8aa14c2fd32e0a915dde124ea6dcc89
OMAR <ojramirezv@gmail.com>
2022-07-21 22:46:10 -0500

    react practice

---
+++ react_with_dependencies/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <h1>My awesome website in React </h1>
+    <h3>Reasons I love react</h3>
+    <ol>
+      <li>It's composable</li>
+      <li>It's declarative</li>
+      <li>It's hireable skill</li>
+      <li>It's actively maintained by skilled people</li>
+    </ol>
+  </div>
+  );
+}
+
+
+export default App;
